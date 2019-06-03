c7230f5b84e3627c70f36cb2d05e3c8911cfaaa9
yuvalmor94 <yuvalmor94@gmail.com>
2019-06-03 18:08:56 +0300

    adding consts and Client

--- Exercise3/Models/Client.cs
+++ Exercise3/Models/Client.cs
@@ -1,11 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Net.Sockets;
+using System.Text;
 
 namespace Exercise3.Models
 {
     public class Client
     {
+        private static Client s_instace = null;
+        private static TcpClient client;
+        private bool connected = false;
+
+        public static Client Instance
+        {
+            get
+            {
+                if (s_instace == null)
+                {
+                    s_instace = new Client();
+                    client = new TcpClient();
+                }
+                return s_instace;
+            }
+        }
+
+
+        public void Connect(string ip, int port)
+        {
+            while (!connected)
+            {
+                try
+                {
+                    client.Connect(ip, port);
+                    connected = true;
+                    break;
+                }
+                catch { }
+            }
+        }
+
+        public string GetLine(string kind)
+        {
+            string request = "get ";
+            switch (kind)
+            {
+                case "longitude":
+                    request += Consts.LONGIDUTE_XML;
+                    break;
+                case "latitude":
+                    request += Consts.LATITUDE_XML;
+                    break;
+                case "rudder":
+                    request += Consts.RUDDER_XML;
+                    break;
+                case "throttle":
+                    request += Consts.THROTTLE_XML;
+                    break;
+                default:
+                    return "";
+
+            }
+
+            request += Consts.NEW_LINE;
+            return request;
+        }
+        public string GetData(string line)
+        {
+            string info = "";
+            try
+            {
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] buffer = asen.GetBytes(line);
+                NetworkStream stream = client.GetStream();
+                stream.Write(buffer, 0, buffer.Length);
+
+                byte[] readBytes = new byte[client.ReceiveBufferSize];
+                int sizeRead = stream.Read(readBytes, 0, client.ReceiveBufferSize);
+                if (sizeRead > 0)
+                {
+                    info = System.Text.Encoding.UTF8.GetString(readBytes, 0, readBytes.Length);
+                    string[] splitInfo = info.Split('\'');
+                    info = splitInfo[1];
+                }
+                else
+                {
+                    // print message?
+                }
+
+            }
+            catch { }
+
+            return info;
+
+        }
+
     }
 }
---
+++ Exercise3/Models/Consts.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Exercise3.Models
+{
+    public class Consts
+    {
+        public const int BUFFER_SIZE = 1024;
+        public const int READ_FAILED = -1;
+        public const string LON_XML = "longitude-deg";
+        public const string LAT_XML = "latitude-deg";
+        public const string THROTTLE_XML = "/controls/engines/current-engine/throttle";
+        public const string RUDDER_XML = "/controls/flight/rudder";
+        public const string LATITUDE_XML = "/position/latitude-deg";
+        public const string LONGIDUTE_XML = "/position/longitude-deg";
+        public const string NEW_LINE = "\r\n";
+    }
+}
